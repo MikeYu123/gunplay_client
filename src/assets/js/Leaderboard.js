75e5f55f6a03c8aad40dc95012ccfe383de67ec9
a.rudenko <a.rudenko@maximatelecom.ru>
2019-01-14 15:20:55 +0300

    i don't know how to use unicode

--- src/assets/js/Leaderboard.js
+++ src/assets/js/Leaderboard.js
@@ -22,9 +22,10 @@
 
         data
             .sort( (a, b) => a - b )
-            .forEach( ({id, name, kills, deaths}) => {
+            .forEach( ({id, name, kills, deaths}, idx) => {
+                let additionalSymb = idx === 0 ? '👑' : '';
                 this.table.innerHTML += `<tr class="${id}">
-                    <td class="name">${name}</td>
+                    <td class="name">${additionalSymb}${name}</td>
                     <td>${kills}</td>
                     <td>${deaths}</td>
                 </tr>`;
