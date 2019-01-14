df3c45197edfff276faf49af6ea63c4a32365966
Yurchenkov Mikhail <m.yurchenkov@maximatelecom.ru>
2019-01-14 15:46:10 +0300

    styling a bit

--- src/assets/js/Leaderboard.js
+++ src/assets/js/Leaderboard.js
@@ -7,6 +7,8 @@
     render() {
         this.el = document.createElement('div');
         this.table = document.createElement('table');
+        Object.assign(this.el.style, {width: '200px'});
+        Object.assign(this.table.style, {width: '200px'});
 
         this.el.className = 'leaderboard';
 
@@ -19,16 +21,23 @@
 
     update( data ) {
         this.clean();
-
+        this.table.innerHTML += "<theader><tr>" +
+            "<th>Name</th>" +
+            "<th>Kills</th> " +
+            "<th>Deaths</th> " +
+            "</tr></theader>"
+        this.table.innerHTML += '<tbody>'
         data
             .sort( (a, b) => a - b )
             .forEach( ({id, name, kills, deaths}) => {
                 this.table.innerHTML += `<tr class="${id}">
                     <td class="name">${name}</td>
-                    <td>${kills}</td>
-                    <td>${deaths}</td>
+                    <td >${kills}</td>
+                    <td >${deaths}</td>
                 </tr>`;
             });
+        this.table.innerHTML += '</tbody>'
+
     }
 
     clean() {
