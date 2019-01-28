a0e45760d6d984daba7385209bdce4b8f3db4dd9
Yurchenkov Mikhail <m.yurchenkov@maximatelecom.ru>
2019-01-28 15:29:22 +0300

    Configs pt. 4

--- src/assets/js/core/WorldUpdater.js
+++ src/assets/js/core/WorldUpdater.js
@@ -9,36 +9,37 @@ export default class WorldUpdater {
     constructor({ world, player }) {
         this.world = world;
         this.player = player;
-    }
+        this.update = (message) =>
+            {
+                const {type} = message;
+                switch (type) {
+                    case "updates":
+                        const {bodies, bullets, doors, player} = message;
+                        this.world.flush();
+                        bodies.forEach(body => {
+                            this.world.addBody(body, false);
+                        });
+                        bullets.forEach(bullet => {
+                            this.world.addBullet(bullet);
+                        });
+                        doors.forEach(door => {
+                            this.world.addDoor(door);
+                        });
+                        if (player) {
+                            this.world.addBody(player, true);
+                            this.world.resetCenter(player);
 
-    update(message) {
-        const { type } = message;
-        switch (type) {
-            case "updates":
-                const {bodies, bullets, doors, player} = message;
-                this.world.flush();
-                bodies.forEach(body => {
-                    this.world.addBody(body, false);
-                });
-                bullets.forEach(bullet => {
-                    this.world.addBullet(bullet);
-                });
-                doors.forEach(door => {
-                    this.world.addDoor(door);
-                });
-                if(player) {
-                    this.world.addBody(player, true);
-                    this.world.resetCenter(player);
+                        }
+                        this.world.refresh();
+                        break;
+                    case "leaderboard":
+                        debounce(LeadBoard.update(message.leaderboard), leaderboardDefaults.debounce.timeout)
+                        break;
+                    case "registered":
+                        this.player.uuid = message.id;
 
                 }
-                this.world.refresh();
-                break;
-            case "leaderboard":
-                debounce(LeadBoard.update( message.leaderboard ), leaderboardDefaults.debounce.timeout)
-                break;
-            case "registered":
-                this.player.uuid = message.id;
-
-        }
+            }
     }
+
 }
