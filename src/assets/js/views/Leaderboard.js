136421c781011609cd4f1131d4094daedc8d2975
Yurchenkov Mikhail <m.yurchenkov@maximatelecom.ru>
2019-01-28 15:39:58 +0300

    Configs pt. 6

--- src/assets/js/views/Leaderboard.js
+++ src/assets/js/views/Leaderboard.js
@@ -1,46 +1,47 @@
  export default class Leaderboard{
     constructor({host}){
         this.host = host || window.document.documentElement;
-        this.render()
-    }
+        this.render = () => {
+            this.el = document.createElement('div');
+            this.table = document.createElement('table');
+            Object.assign(this.el.style, {width: '200px'});
+            Object.assign(this.table.style, {width: '200px'});
 
-    render() {
-        this.el = document.createElement('div');
-        this.table = document.createElement('table');
-        Object.assign(this.el.style, {width: '200px'});
-        Object.assign(this.table.style, {width: '200px'});
+            this.el.className = 'leaderboard';
 
-        this.el.className = 'leaderboard';
+            this.host.appendChild( this.el );
+            this.el.appendChild(this.table)
 
-        this.host.appendChild( this.el );
-        this.el.appendChild(this.table)
+            //TODO: move to style files this shit
 
-        //TODO: move to style files this shit
+        };
 
-    }
+        const clean = () => {
+            this.table.innerHTML = ''
+        };
 
-    update( data ) {
-        this.clean();
-        this.table.innerHTML += "<theader><tr>" +
-            "<th>Name</th>" +
-            "<th>Kills</th> " +
-            "<th>Deaths</th> " +
-            "</tr></theader>"
-        this.table.innerHTML += '<tbody>'
-        data
-            .sort( (a, b) => a - b )
-            .forEach( ({id, name, kills, deaths}) => {
-                this.table.innerHTML += `<tr class="${id}">
+        this.update = data => {
+            clean();
+            this.table.innerHTML += "<theader><tr>" +
+                "<th>Name</th>" +
+                "<th>Kills</th> " +
+                "<th>Deaths</th> " +
+                "</tr></theader>"
+            this.table.innerHTML += '<tbody>'
+            data
+                .sort( (a, b) => a - b )
+                .forEach( ({id, name, kills, deaths}) => {
+                    this.table.innerHTML += `<tr class="${id}">
                     <td class="name">${name}</td>
                     <td >${kills}</td>
                     <td >${deaths}</td>
                 </tr>`;
-            });
-        this.table.innerHTML += '</tbody>'
+                });
+            this.table.innerHTML += '</tbody>'
 
+        }
+
+        this.render()
     }
 
-    clean() {
-        this.table.innerHTML = ''
-    }
  }
